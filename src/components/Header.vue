cabfcd1721784c31ad655ad5d7ded7aaa24a07d8
Wesley Goarant <Wesleydu29860@gmail.com>
2025-03-24 21:04:50 +0100

    début du composant Header

---
+++ src/components/Header.vue
@@ -0,0 +1,37 @@
+<template>
+    <header>
+        <h1>Bienvenu sur mon portfolio</h1>
+        <nav>
+            <ul>
+                <li><a href="#" class="hover:underline">Accueil</a></li>
+                <li><a href="#" class="hover:underline">Projets</a></li>
+                <li><a href="#" class="hover:underline">Contact</a></li>
+            </ul>
+        </nav>
+    </header>
+
+</template>
+
+
+<script setup> 
+
+</script>
+
+
+<style scoped>
+header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+}
+a {
+    color: #000000;
+    text-decoration: none;
+    font-size: 1.3rem;
+}
+ul {
+    display: flex;
+    gap: 10px;
+    list-style: none;
+}
+</style>
