c76ef44abb9d33fc9afee9bed6b257e12d5a5d59
Wesley Goarant <Wesleydu29860@gmail.com>
2025-05-17 22:30:19 +0200

    ajout d'un footer

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <template>
     <Header/>
+    <Footer/>
   
 </template>
 
 <script setup> 
+import Footer from './components/Footer.vue';
 
 import Header from './components/Header.vue'
 </script>
---
+++ src/components/Footer.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="footer">
+        <p>Goarant Wesley. Front-end Developper</p>
+    </div>
+
+</template>
+<script setup>
+
+</script>
+
+<style scoped>
+.footer {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-size: 1.2rem;
+    
+}
+</style>
